a3ec2d8ea03022d4c79e0f723c24f7234027f443
Aditya <aditya21511@iiitd.ac.in>
2022-06-24 23:24:22 +0530

    shifted the variables to the end and added binary encoding of registers

--- input.py
+++ input.py
@@ -9,4 +9,10 @@ def takeinput():
     for i in d:
         if i.split()!=[]:
             instructions.append(i.split())
+    for i in instructions:
+        if (i[0]=="var"):
+            instructions.append(instructions.pop(0))
     f.close()
+def encodeRegister():
+    reg={"R0":"000", "R1":"001", "R2":"010", "R3":"011", "R4":"100","R5":"101", "R6":"110", "FLAGS":"111"}
+    return reg
