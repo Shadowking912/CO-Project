22c6c4451af61b51ced825a62cf64371ffc59631
Aditya <aditya21511@iiitd.ac.in>
2022-07-03 20:09:18 +0530

    changed input.py to take input from terminal

--- input.py
+++ input.py
@@ -1,18 +1,35 @@
-def takeinput():
-    f=open("input.txt","r")
-    d=f.readlines()
-    d="".join(d)
-    d=d.split("\n")
-    global instructions
+import sys
+def takeInput():
+    d=sys.stdin.readlines()
     instructions=[]
-    print(d)
-    for i in d:
-        if i.split()!=[]:
-            instructions.append(i.split())
-    for i in instructions:
-        if (i[0]=="var"):
+    index=[]
+    h=0
+    v=0
+    for i in range(len(d)):
+        if (d[i].split()!=[]):
+            instructions.append(d[i].strip().split())
+            if("".join(instructions[-1])[0:3]=="var"):
+                v+=1
+            index.append(i)
+        if("hlt" in d[i]):
+            if(i<len(d)-1):
+                h=1
+                print(f"Error at line no {i+1}: hlt not at end of program")
+                exit(0)
+            elif(h==1):
+                print("More than one hlt")
+                exit(0)
+            else:
+                h=1
+    if(h==0):
+        print("Error: missing hlt instruction")
+        exit(0)
+    i=0
+    labels,variables=LABL(instructions,index,len(instructions),v)
+    while i<len(instructions):
+        if (instructions[i][0]=="var"):
             instructions.append(instructions.pop(0))
-    f.close()
-def encodeRegister():
-    reg={"R0":"000", "R1":"001", "R2":"010", "R3":"011", "R4":"100","R5":"101", "R6":"110", "FLAGS":"111"}
-    return reg
+            index.append(index.pop(0))
+        else:
+            break
+    return [instructions,labels,variables,index]
